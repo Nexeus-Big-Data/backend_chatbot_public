315fe0d294b90ed547b9d0bee1ddb63e4bab5fef
angeloiro <angelloirogarcia@hotmail.com>
2025-03-31 15:54:11 +0200

    Actualizar main.py

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Servir archivos estáticos (como HTML, CSS, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
